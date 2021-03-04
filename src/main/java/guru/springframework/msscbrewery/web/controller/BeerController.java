c4c74aa48e4bdbcc0040295e7edb14bf15a935c8
Albert So <aso@atlantistech.com>
2021-03-04 10:56:27 -0500

    Add delete beer endpoint

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -41,4 +41,10 @@ public class BeerController {
 
         return new ResponseEntity(HttpStatus.NO_CONTENT); // 204, successful & understood
     }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable UUID beerId) {
+        beerService.deleteById(beerId);
+    }
 }
